7e76ce0b2aa12761187741a965620e7418d9ac45
Herzu <dawidherc@gmail.com>
2021-04-28 21:33:43 +0200

    dodanie możliwości tworzenia trójwymiarowej mapy

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class GameController: MonoBehaviour
+public class GameController : MonoBehaviour
 {
     public GameObject blockPrefab;
     public GameObject wallPrefab;
@@ -10,15 +10,21 @@ public class GameController: MonoBehaviour
     public Terrain terrain;
     public int mapXSize;
     public int mapYSize;
+    public int mapZSize = 1;
     // Start is called before the first frame update
     void Start()
     {
         powerups = new Queue<Vector3>();
+        terrain.terrainData.size = new Vector3(2 * mapXSize, 0, 2 * mapYSize);
+        if (mapZSize == 1)
+            Fill2D();
+        else
+            Fill3D();
+    }
+    void Fill2D()
+    {
         int chance = 25;
-        int realXSize = 2 * mapXSize;
-        int realYSize = 2 * mapYSize;
-        terrain.terrainData.size = new Vector3(realXSize, 0, realYSize);
-        for(int i=0;i<mapXSize;i++)
+        for (int i = 0; i < mapXSize; i++)
         {
             for (int j = 0; j < mapYSize; j++)
             {
@@ -38,11 +44,34 @@ public class GameController: MonoBehaviour
             }
         }
     }
+    void Fill3D()
+    {
+        int chance = 25;
+        for (int i = 0; i < mapXSize; i++)
+        {
+            for (int j = 0; j < mapYSize; j++)
+            {
+                for (int k = 0; k < mapZSize; k++)
+                    if (i == 0 || j == 0 || i == mapXSize - 1 || j == mapYSize - 1)
+                        Instantiate(wallPrefab, new Vector3(2 * i + 1, 2 * k + 1, 2 * j + 1), Quaternion.identity);
+                    else if ((i > 2 || j > 2)
+                        && (i < mapXSize - 3 || j < mapYSize - 3)
+                        && (i > 2 || j < mapYSize - 3)
+                        && (i < mapXSize - 3 || j > 2))
+                    {
+                        if (i % 2 == 0 && j % 2 == 0)
+                            Instantiate(wallPrefab, new Vector3(2 * i + 1, 2 * k + 1, 2 * j + 1), Quaternion.identity);
+                        else if (chance >= Random.Range(1, 100))
+                            Instantiate(blockPrefab, new Vector3(2 * i + 1, 2 * k + 1, 2 * j + 1), Quaternion.identity).gameObject.GetComponent<BlockDestroy>().GameController = this;
+                    }
 
+            }
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        while(powerups.Count>0)
+        while (powerups.Count > 0)
         {
             Vector3 position = powerups.Dequeue();
             int rand = Random.Range(1, 100);
@@ -56,7 +85,7 @@ public class GameController: MonoBehaviour
                     break;
                 }
             }
-        }    
+        }
     }
 
     public void AddPowerup(Vector3 position)
