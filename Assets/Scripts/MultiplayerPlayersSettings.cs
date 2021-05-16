4f040133ec0ae0155b39039e84afabc23abf2056
Tomasz Knura <tomasz.knura@itsilesia.com>
2021-05-16 17:44:13 +0200

    Dodano ustawienia hot seat (nie skonczone)

---
+++ Assets/Scripts/MultiplayerPlayersSettings.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ControlType {
+    None = 0,
+    WSAD = 1,
+    Arrows = 2,
+    Controller = 3,
+    AI = 4
+}
+
+public class MultiplayerPlayersSettings : MonoBehaviour
+{
+    private ControlType[] playerControls;
+
+    public void setControlType(int playerNumber, ControlType type) {
+        playerControls[playerNumber - 1] = type;
+    }
+}
---
+++ Assets/Scripts/PlayerSelector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerSelector : MonoBehaviour
+{
+    public Color color;
+    public Color disabledColor;
+    public GameObject frame;
+
+    public void handleToggleUI(bool value) {
+        Image backgroundFrame = frame.GetComponent<Image>();
+        if(value) {
+            backgroundFrame.color = color;
+        } else {
+            backgroundFrame.color = disabledColor;
+        }
+    }
+}
