90ec10788fc6645b243e60df52c398e388a8505b
Tomasz Knura <tomasz.knura@itsilesia.com>
2021-05-23 18:18:56 +0200

    Zakomentowano PlayerSelector

--- Assets/Scripts/UIScripts/PlayerSelector.cs
+++ Assets/Scripts/UIScripts/PlayerSelector.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+//! Klasa odpowiedzialna za komponent wyboru rodzaju sterowania gracza
 public class PlayerSelector : MonoBehaviour
 {
-    public Color color;
-    public Color disabledColor;
-    public GameObject frame;
+    public Color color;         //!< kolor zaznaczenia
+    public Color disabledColor; //!< kolor wylaczony
+    public GameObject frame;    //!< obiekt przechowujacy ramke
 
+	/** Funkcja obslugujaca toggle
+	 * @param value nowa wartosc toggle'a
+	 */
     public void handleToggleUI(bool value) {
         Image backgroundFrame = frame.GetComponent<Image>();
         if(value) {
