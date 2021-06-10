396f2fa576f916f87c03438aa655a67ee6d496a2
Herzu <dawidherc@gmail.com>
2021-06-10 22:44:07 +0200

    presentation version

--- Projekt/Assets/Scripts/BomberAgent.cs
+++ Projekt/Assets/Scripts/BomberAgent.cs
@@ -7,15 +7,35 @@ using Unity.MLAgents.Sensors;
 public class BomberAgent : Agent
 {
     public GameObject target;
+    public float previousScore;
 
-    public override void CollectObservations(VectorSensor sensor) {}
+    public override void CollectObservations(VectorSensor sensor) 
+    {
+        sensor.AddObservation(gameObject.transform.InverseTransformPoint(target.transform.position));
+        sensor.AddObservation(gameObject.GetComponent<Rigidbody>().velocity);
+    }
 
     public override void OnActionReceived(float[] actionBuffers)
     {
         var moveX = actionBuffers[0] - 1;
         var moveZ = actionBuffers[1] - 1;
-
-        gameObject.GetComponent<Rigidbody>().velocity = new Vector3(moveX, 0, moveZ);
-        SetReward(1.0f / ((gameObject.transform.position - target.transform.position).magnitude) + 1.0f);
+        Debug.Log(moveX.ToString()+";"+moveZ.ToString());
+        gameObject.GetComponent<Rigidbody>().velocity = new Vector3(5*moveX, 0, 5*moveZ);
+        float newScore = (1.0f / (((gameObject.transform.position - target.transform.position).magnitude) + 1.0f));
+        if (newScore > previousScore+0.0005f)
+        {
+            previousScore = newScore;
+            AddReward(1.0f);
+        }
+        if(newScore<previousScore)
+        {
+            previousScore = newScore;
+            AddReward(-1.0f);
+        }
+    }
+    public override void OnEpisodeBegin()
+    {
+        previousScore = 0;
+        gameObject.transform.position = new Vector3Int(13, 1, 13);
     }
 }
